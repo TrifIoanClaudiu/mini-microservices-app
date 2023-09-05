9a73e53b6fc362dbac75b80ff5c1680ccd663744
Ionut Trif <trifionut207@gmail.com>
2023-09-05 14:50:38 +0300

    Add files

---
+++ event-bus/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+const events = [];
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  try {
+    axios.post("http://localhost:4000/events", event).catch((err)=> {console.log(err)});
+    axios.post("http://localhost:4001/events", event).catch((err)=> {console.log(err)});
+    axios.post("http://localhost:4002/events", event).catch((err)=> {console.log(err)});
+    axios.post("http://localhost:4003/events", event).catch((err)=> {console.log(err)});
+
+    res.send({ status: "OK" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get('/events', (req,res) => {
+  res.send(events);
+})
+
+app.listen(4005, () => {
+  console.log("Server is running on port 4005");
+})
